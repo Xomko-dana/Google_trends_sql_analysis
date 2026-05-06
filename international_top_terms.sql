7d6013255819ec4d20783784dfce11dace357489
Dana Mu <67094814+Xomko-dana@users.noreply.github.com>
2026-05-06 11:00:16 +0300

    Add SQL queries for international top terms analysis
    Added SQL queries to retrieve top terms, calculate date range, and preview sample data.

--- international_top_terms.sql
+++ international_top_terms.sql
@@ -1,11 +1,13 @@
---
+-- Retrieve top international trending search terms for a given date
 SELECT  FROM `bigquery-public-data.google_trends.international_top_terms`
 WHERE refresh_date = "2026-05-06" 
 LIMIT 1000
---
+  
+-- Calculate min and max refresh dates for international trends
 SELECT MAX(refresh_date) AS last_refresh_date, MIN(refresh_date) AS first_refresh_date
 FROM bigquery-public-data.google_trends.international_top_terms
---
+  
+-- Preview sample data from international trends dataset
 SELECT *
 FROM bigquery-public-data.google_trends.international_top_terms
 LIMIT 5
