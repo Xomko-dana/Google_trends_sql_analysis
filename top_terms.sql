9ac91725741f37803baf819d4aeac0fb7e0d72c8
Dana Mu <67094814+Xomko-dana@users.noreply.github.com>
2026-05-06 10:45:38 +0300

    Add query for first and last refresh dates
    Added a query to retrieve the first and last refresh dates of top trending terms.

--- top_terms.sql
+++ top_terms.sql
@@ -8,3 +8,8 @@ FROM `bigquery-public-data.google_trends.top_terms`
 WHERE refresh_date = "2026-05-05"
 ORDER BY score DESC
 LIMIT 100;
+
+-- Top trending terms with function and alias
+SELECT MAX(refresh_date) AS last_refresh_date, MIN(refresh_date) AS first_refresh_date
+FROM bigquery-public-data.google_trends.top_terms
+LIMIT 5
